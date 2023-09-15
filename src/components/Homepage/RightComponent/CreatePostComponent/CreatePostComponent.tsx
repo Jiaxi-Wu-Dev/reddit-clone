f31e623c8e080cb716b10de0e342045eab5078b4
Jiaxi Wu <61803844+Jiaxi-Wu-Dev@users.noreply.github.com>
2023-09-14 21:26:12 -0500

    working on subRedditDescriptionText section

--- src/components/Homepage/RightComponent/CreatePostComponent/CreatePostComponent.tsx
+++ src/components/Homepage/RightComponent/CreatePostComponent/CreatePostComponent.tsx
@@ -12,6 +12,20 @@ export default function CreatePostComponent() {
             <span id="createPostComponentText">r/all</span>
           </div>
         </div>
+        <div id="subRedditDescriptionTextSection">
+          <div id="subRedditDescriptionText">
+            <div id="subRedditDescriptionTextLine">
+              The most active posts from all of Reddit.
+            </div>
+            <div id="subRedditDescriptionTextLine">
+              Come here to see new posts rising and be a
+            </div>
+            <div id="subRedditDescriptionTextLine">
+              part of the conversation.
+            </div>
+            <hr id="lineSeperator"></hr>
+          </div>
+        </div>
       </div>
     </div>
   );
