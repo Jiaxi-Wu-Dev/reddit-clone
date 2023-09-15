d0f13e25c88c2bee9343ff7cf55e6490a54d532d
Jiaxi Wu <61803844+Jiaxi-Wu-Dev@users.noreply.github.com>
2023-09-15 15:59:51 -0500

    recent posts

--- src/components/Homepage/RightComponent/RecentPostsComponent/RecentPostsComponent.tsx
+++ src/components/Homepage/RightComponent/RecentPostsComponent/RecentPostsComponent.tsx
@@ -7,7 +7,19 @@ export default function RecentPostsComponent() {
       <div>
         <div id="recentPostsInnerComponent">
           <div>
-            <div id="recentPostsInnerComponentTitle">Recent Posts</div>
+            <div id="recentPostsInnerComponentTitle">
+              <h2>Recent Posts</h2>
+            </div>
+            <div>
+              <div id="recentPostsPictureText">
+                <div id="recentPostsInnerComponentPicture">
+                  <img></img>
+                </div>
+                <div id="recentPostsInnerComponentText">
+                  <p>Dude almost choked to death by barbell</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
