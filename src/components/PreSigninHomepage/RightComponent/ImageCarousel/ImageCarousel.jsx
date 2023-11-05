f296deefc18ef0c13d68d77cd16ab3f38629e766
Jiaxi Wu <61803844+Jiaxi-Wu-Dev@users.noreply.github.com>
2023-11-05 11:26:25 -0600

    finished setting up the carousel list with pictures and details

--- src/components/PreSigninHomepage/RightComponent/ImageCarousel/ImageCarousel.jsx
+++ src/components/PreSigninHomepage/RightComponent/ImageCarousel/ImageCarousel.jsx
@@ -23,7 +23,18 @@ function ImageCarousel() {
                   <p className="carouselTextPara">
                     [NFL] NFL Power Rankings, Week 9:
                   </p>
-                  <p className="carouselTextPara">r/nfl</p>
+                  <div className="circlePhotoContainer">
+                    <span className="circlePhotoSpan">
+                      <div className="circleDiv">
+                        <img
+                          src="https://styles.redditmedia.com/t5_2qmg3/styles/communityIcon_okwiq8ji95ab1.png"
+                          className="circleImage"
+                          alt="football"
+                        />
+                      </div>
+                    </span>
+                    <p className="carouselTextPara">r/nfl</p>
+                  </div>
                 </div>
               </div>
             </Box>
@@ -32,21 +43,32 @@ function ImageCarousel() {
         <li className="carouselList">
           <a
             className="carouselLink"
-            href="https://www.reddit.com/search/?q=NFL+Week+9&type=link&source=trending&cId=473cc073-44b0-4c36-9568-60f07dc5aaa2&iId=0e1ce4f8-66f3-4c78-87ee-eb2bab745470"
+            href="https://www.reddit.com/search/?q=subreddit%3Aformula1&type=link&source=trending&cId=ebf853cf-261f-41e6-9e65-56dab2afb470&iId=c09f8879-703f-45f9-9513-396b34242960"
           >
             <Box className="carouselBox">
               <img
-                src="https://external-preview.redd.it/nfl-nfl-power-rankings-week-9-eagles-ravens-and-dolphins-v0-bA6O4TVnuzyAbRMXOMkJM3mdO3oYOpWQ90pAGqwmtEo.jpg?width=320&crop=smart&auto=webp&s=16435e438df5f3fab20241aea96e05b3f0f528eb"
+                src="https://external-preview.redd.it/alonso-whenever-there-is-an-alpine-you-have-to-be-extra-v0-GNf5jXGkNTwdMp7ZUUAzASVafRnmyJGBpuGcs7hPwEk.jpg?width=320&crop=smart&auto=webp&s=39513d02c325bf8571f4d1f282d6255268c88cb4"
                 alt="nfl week 9"
                 className="carouselImage"
               />
               <div className="carouselTextContainer">
                 <div className="carouselText">
-                  <h1 className="carouselTextPara">NFL WEEK 9</h1>
+                  <h1 className="carouselTextPara">Grand Prix</h1>
                   <p className="carouselTextPara">
-                    [NFL] NFL Power Rankings, Week 9:
+                    Alonso: "Whenever there is an Alpine year"
                   </p>
-                  <p className="carouselTextPara">r/nfl</p>
+                  <div className="circlePhotoContainer">
+                    <span className="circlePhotoSpan">
+                      <div className="circleDiv">
+                        <img
+                          src="https://styles.redditmedia.com/t5_2qimj/styles/communityIcon_g52c3tratr9b1.png"
+                          className="circleImage"
+                          alt="formula 1"
+                        />
+                      </div>
+                    </span>
+                    <p className="carouselTextPara">r/formula1</p>
+                  </div>
                 </div>
               </div>
             </Box>
@@ -55,21 +77,32 @@ function ImageCarousel() {
         <li className="carouselList">
           <a
             className="carouselLink"
-            href="https://www.reddit.com/search/?q=NFL+Week+9&type=link&source=trending&cId=473cc073-44b0-4c36-9568-60f07dc5aaa2&iId=0e1ce4f8-66f3-4c78-87ee-eb2bab745470"
+            href="https://www.reddit.com/search/?q=subreddit%3Anba&type=link&source=trending&cId=ebf853cf-261f-41e6-9e65-56dab2afb470&iId=219996a8-509a-45ef-a3c1-531cb943d1c5"
           >
             <Box className="carouselBox">
               <img
-                src="https://external-preview.redd.it/nfl-nfl-power-rankings-week-9-eagles-ravens-and-dolphins-v0-bA6O4TVnuzyAbRMXOMkJM3mdO3oYOpWQ90pAGqwmtEo.jpg?width=320&crop=smart&auto=webp&s=16435e438df5f3fab20241aea96e05b3f0f528eb"
+                src="https://external-preview.redd.it/refs-throw-a-completely-fair-and-perfect-jump-ball-v0-g-HNxDNl-dUJHEzdaPji3jOro-cUeWZBE-7gE2PPEs8.jpg?width=320&crop=smart&format=pjpg&auto=webp&s=4b2198c1dd612c32b3dc950fbcf8683b986cd218"
                 alt="nfl week 9"
                 className="carouselImage"
               />
               <div className="carouselTextContainer">
                 <div className="carouselText">
-                  <h1 className="carouselTextPara">NFL WEEK 9</h1>
+                  <h1 className="carouselTextPara">NBA</h1>
                   <p className="carouselTextPara">
-                    [NFL] NFL Power Rankings, Week 9:
+                    Refs throw a completely fair and perfect jump ball
                   </p>
-                  <p className="carouselTextPara">r/nfl</p>
+                  <div className="circlePhotoContainer">
+                    <span className="circlePhotoSpan">
+                      <div className="circleDiv">
+                        <img
+                          src="https://styles.redditmedia.com/t5_2qo4s/styles/communityIcon_1podsfdai4301.png"
+                          className="circleImage"
+                          alt="football"
+                        />
+                      </div>
+                    </span>
+                    <p className="carouselTextPara">r/nba</p>
+                  </div>
                 </div>
               </div>
             </Box>
@@ -78,21 +111,32 @@ function ImageCarousel() {
         <li className="carouselList">
           <a
             className="carouselLink"
-            href="https://www.reddit.com/search/?q=NFL+Week+9&type=link&source=trending&cId=473cc073-44b0-4c36-9568-60f07dc5aaa2&iId=0e1ce4f8-66f3-4c78-87ee-eb2bab745470"
+            href="https://www.reddit.com/search/?q=subreddit%3Acricket&type=link&source=trending&cId=ebf853cf-261f-41e6-9e65-56dab2afb470&iId=a596a365-b274-4a65-8c60-a46a1c3e02d4"
           >
             <Box className="carouselBox">
               <img
-                src="https://external-preview.redd.it/nfl-nfl-power-rankings-week-9-eagles-ravens-and-dolphins-v0-bA6O4TVnuzyAbRMXOMkJM3mdO3oYOpWQ90pAGqwmtEo.jpg?width=320&crop=smart&auto=webp&s=16435e438df5f3fab20241aea96e05b3f0f528eb"
+                src="https://preview.redd.it/england-are-officially-knocked-out-of-the-world-cup-v0-c7ioyfal1dyb1.jpg?width=320&crop=smart&auto=webp&s=69b14088a2cc11cf0a6f9b71b23848c5cebf8542"
                 alt="nfl week 9"
                 className="carouselImage"
               />
               <div className="carouselTextContainer">
                 <div className="carouselText">
-                  <h1 className="carouselTextPara">NFL WEEK 9</h1>
+                  <h1 className="carouselTextPara">Cricket World Cup</h1>
                   <p className="carouselTextPara">
-                    [NFL] NFL Power Rankings, Week 9:
+                    England are officially knocked out of the
                   </p>
-                  <p className="carouselTextPara">r/nfl</p>
+                  <div className="circlePhotoContainer">
+                    <span className="circlePhotoSpan">
+                      <div className="circleDiv">
+                        <img
+                          src="https://b.thumbs.redditmedia.com/ht-zG3FvOtDH4eBtq85nfpoSyn5Eoj8gXbwMQTO4QfU.png"
+                          id="circleImage"
+                          alt="football"
+                        />
+                      </div>
+                    </span>
+                    <p className="carouselTextPara">r/Cricket</p>
+                  </div>
                 </div>
               </div>
             </Box>
@@ -101,19 +145,19 @@ function ImageCarousel() {
         <li className="carouselList">
           <a
             className="carouselLink"
-            href="https://www.reddit.com/search/?q=NFL+Week+9&type=link&source=trending&cId=473cc073-44b0-4c36-9568-60f07dc5aaa2&iId=0e1ce4f8-66f3-4c78-87ee-eb2bab745470"
+            href="https://www.reddit.com/search/?q=Fortnite+OG&type=link&source=trending&cId=ebf853cf-261f-41e6-9e65-56dab2afb470&iId=634b29d6-efe1-4744-aaeb-a046ec912c13"
           >
             <Box className="carouselBox">
               <img
-                src="https://external-preview.redd.it/nfl-nfl-power-rankings-week-9-eagles-ravens-and-dolphins-v0-bA6O4TVnuzyAbRMXOMkJM3mdO3oYOpWQ90pAGqwmtEo.jpg?width=320&crop=smart&auto=webp&s=16435e438df5f3fab20241aea96e05b3f0f528eb"
-                alt="nfl week 9"
+                src="https://external-preview..it/nfl-nfl-power-rankings-week-9-eagles-ravens-and-dolphins-v0-bA6O4TVnuzyAbRMXOMkJM3mdO3oYOpWQ90pAGqwmtEo.jpg?width=320&crop=smart&auto=webp&s=16435e438df5f3fab20241aea96e05b3f0f528eb"
+                alt="fortnite"
                 className="carouselImage"
               />
               <div className="carouselTextContainer">
                 <div className="carouselText">
-                  <h1 className="carouselTextPara">NFL WEEK 9</h1>
+                  <h1 className="carouselTextPara">Fortnite OG</h1>
                   <p className="carouselTextPara">
-                    [NFL] NFL Power Rankings, Week 9:
+                    Its time to drop into #FortniteOG!
                   </p>
                   <div className="circlePhotoContainer">
                     <span className="circlePhotoSpan">
@@ -125,7 +169,7 @@ function ImageCarousel() {
                         />
                       </div>
                     </span>
-                    <p className="carouselTextPara">r/nfl</p>
+                    <p className="carouselTextPara">r/FortNiteBR</p>
                   </div>
                 </div>
               </div>
