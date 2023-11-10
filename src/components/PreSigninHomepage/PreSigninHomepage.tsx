2605d7baa0b1ceee58cf16fd410cdefb1ff6b8e7
Jiaxi Wu <61803844+Jiaxi-Wu-Dev@users.noreply.github.com>
2023-11-09 23:03:04 -0600

    fixed border radius bug searchbar

--- src/components/PreSigninHomepage/PreSigninHomepage.tsx
+++ src/components/PreSigninHomepage/PreSigninHomepage.tsx
@@ -1,12 +1,13 @@
 import SearchNavAppBar from "../Appbar/SearchNavAppBar/SearchNavAppBar";
 import LeftComponent from "../PreSigninHomepage/LeftComponent/LeftComponent";
 import RightComponent from "../PreSigninHomepage/RightComponent/RightComponent";
+import PreSigninHomepageNavBar from "../PreSigninHomepage/PreSigninHomePageNavBar/PreSigninHomePageNavBar";
 import "./preSigninHomepage.css";
 
 function PreSigninHomepage() {
   return (
     <div id="preSigninHomepageContainer">
-      <SearchNavAppBar />
+      <PreSigninHomepageNavBar />
       <div id="preSigninHomepage">
         <LeftComponent />
         <RightComponent />
