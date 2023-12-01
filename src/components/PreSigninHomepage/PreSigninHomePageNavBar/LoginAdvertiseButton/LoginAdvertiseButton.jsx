cc336831fc563c326c8ee32578b5abf201596012
Jiaxi Wu <61803844+Jiaxi-Wu-Dev@users.noreply.github.com>
2023-11-30 22:00:32 -0600

    working on modal

--- src/components/PreSigninHomepage/PreSigninHomePageNavBar/LoginAdvertiseButton/LoginAdvertiseButton.jsx
+++ src/components/PreSigninHomepage/PreSigninHomePageNavBar/LoginAdvertiseButton/LoginAdvertiseButton.jsx
@@ -72,9 +72,6 @@ export default function LoginAdvertiseButton() {
             <Typography id="modal-modal-title" variant="h6" component="h2">
               Text in a modal
             </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
           </Box>
         </Modal>
       </div>
