6556162d440e934d3d5f7e7451e84aecd654c354
Jiaxi Wu <61803844+Jiaxi-Wu-Dev@users.noreply.github.com>
2024-02-02 20:14:43 -0600

    adding register/login

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+/* eslint-disable */
 import "./App.css";
 import { useEffect } from "react";
 import PreSigninHomepage from "./components/PreSigninHomepage/PreSigninHomepage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navigation from "./components/Navigation/Navigation";
+import Register from "./components/Signup/Register";
+import Login from "./components/Login/Login";
 
 function App() {
   useEffect(() => {
@@ -12,6 +17,15 @@ function App() {
   return (
     <>
       <PreSigninHomepage />
+      <Router>
+        <div>
+          <Navigation />
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+          </Switch>
+        </div>
+      </Router>
     </>
   );
 }
